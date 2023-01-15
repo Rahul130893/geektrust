c4378edefe4120f4f39d6f83da3cbaf48845cb47
Rahul <rahulrdx1308@gmail.com>
2023-01-16 03:44:18 +0530

    total amount

--- geektrust_frontend/src/Components/SingleCart.jsx
+++ geektrust_frontend/src/Components/SingleCart.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 import styles from "./SingleProduct.module.css";
 import { CartContext } from "../Context/CartContext";
 import { useContext } from "react";
 
-
 export const SingleCart = ({ props }) => {
-        const { name, price, imageURL, id } = props;
- const { state, dispatch } = useContext(CartContext);
+  const { name, price, imageURL, id } = props;
+  const { state, dispatch } = useContext(CartContext);
 
   //  removeItem(index) {
   //     const todos = this.state.todos.filter((todo, todoIndex) => {
@@ -14,28 +13,30 @@ export const SingleCart = ({ props }) => {
   //     })
   //     this.setState({ todos })
   //   }
-    const RemoveFromCart = (id) => {
-       // console.log(state)
-        const newState = state.cart.filter((e, i) => {
-            return e.id !== id
-        })
-        console.log(newState)
-       dispatch({
-         type: "DELETE_FROM_CART",
-         payload: newState,
-       });
+  const RemoveFromCart = (id) => {
+    // console.log(state)
+    const newState = state.cart.filter((e, i) => {
+      return e.id !== id;
+    });
+    console.log(newState);
+    dispatch({
+      type: "DELETE_FROM_CART",
+      payload: newState,
+    });
     //    alert("added");
-    };
-    
-    console.log(state)
-    return (
-      <div className={styles.cart}>
+  };
+
+  console.log(state);
+  return (
+    <div className={styles.checkout}>
+      <img src={imageURL} alt={"imageLink"} />
+      <div>
+        <p>Rs: {price}</p>
         <p>{name}</p>
-        <img src={imageURL} alt={"imageLink"} />
-        <div>
-          <p>Rs: {price}</p>
-          <button onClick={() => RemoveFromCart(id)}>REMOVE</button>
-        </div>
       </div>
-    );
-}
+      <div>
+        <button onClick={() => RemoveFromCart(id)}>REMOVE</button>
+      </div>
+    </div>
+  );
+};
--- geektrust_frontend/src/pages/Cart.jsx
+++ geektrust_frontend/src/pages/Cart.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CartContext } from "../Context/CartContext";
 import { useContext } from "react";
 import { SingleCart } from '../Components/SingleCart';
 export const Cart = () => {
      const { state, dispatch } = useContext(CartContext);
-
+    const [total, setTotal]= useState(0)
     
     console.log(state.cart)
+
+    useEffect(() => {
+        let sum=0
+        state.cart?.map((e) => {
+            sum+= e.price
+        })
+
+        console.log("price", sum)
+        setTotal(sum)
+    },[state])
+
   return (
       <div>
           {state.cart?.map((e) => {
@@ -16,6 +27,8 @@ export const Cart = () => {
                 </div>
               );
           })}
+
+          <p>Total: {total}</p>
     </div>
   )
 }
